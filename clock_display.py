bd3830dc370261d8d13e3b6f11558492f271f62b
chip <chip@example.com>
2021-07-10 13:13:07 -0400

    Working candidate for signal handler.

--- clock_display.py
+++ clock_display.py
@@ -6,6 +6,7 @@
 import i2clcd
 import subprocess
 import sys
+import signal
 from datetime import datetime
 from time import sleep
 
@@ -13,43 +14,50 @@ from time import sleep
 #     dates supplied are just fillers - they are ignored during actual
 #     execution.
 light_on  = datetime(1970, 1, 1,  6,  0, 0)  # Time the backlight should be switched on
-light_off = datetime(1970, 1, 1, 22, 30, 0) # Time the backlight should be switched off
+light_off = datetime(1970, 1, 1, 22, 30, 0)  # Time the backlight should be switched off
 
-# Initialization
+
+# FUNCTIONS
+def closeDisplay():
+    # Gracefully stops operation by blanking the screen and turning the
+    # backlight off.
+    lcd.print_line("                ", 0)  # For some reason the clear() function
+    lcd.print_line("                ", 1)  # doesn't work, so I print blank lines
+    lcd.set_backlight(False)               # instead.
+    print ("")
+    sys.exit()
+
+
+# INITIALIZATION
 lcd = i2clcd.i2clcd(i2c_bus=1, i2c_addr=0x27, lcd_width=16)
 lcd.init()
 
 now = datetime.now()
 timestr = now.strftime("%H:%M:%S")
 datestr = now.strftime("%b %d, %Y")
-line0, line1 = "INITIALIZING...", "INITIALIZING..."
+line0 = "INITIALIZING..."
 prettyAcc = "    INIT"
 
+signal.signal(signal.SIGTERM, closeDisplay)
+
+
+# MAIN LOOP
 while True:
-    try:
-        now = datetime.now()
+    now = datetime.now()
 
-        if timestr != now.strftime("%H:%M:%S"):
-            timestr = now.strftime("%H:%M:%S")
-            if now.strftime("%S") == "00":
-                accReport = subprocess.getoutput("ntpstat | grep within | cut -d ' ' -f 8")
-                prettyAcc = "{message: >8}".format(message=accReport + "ms")
-            line0 = timestr + prettyAcc
+    if timestr != now.strftime("%H:%M:%S"):
+        timestr = now.strftime("%H:%M:%S")
+        if now.strftime("%S") == "00":
+            accReport = subprocess.getoutput("ntpstat | grep within | cut -d ' ' -f 8")
+            prettyAcc = "{message: >8}".format(message=accReport + "ms")
+        line0 = timestr + prettyAcc
         
-        datestr = now.strftime("%b %d, %Y")
+    datestr = now.strftime("%b %d, %Y")
 
-        if light_on.time() <= now.time() <= light_off.time():
-            lcd.set_backlight(True)
-        else:
-            lcd.set_backlight(False)
+    if light_on.time() <= now.time() <= light_off.time():
+        lcd.set_backlight(True)
+    else:
+        lcd.set_backlight(False)
 
-        lcd.print_line(line0, 0)
-        lcd.print_line(datestr, 1)
-    except:
-        break
-
-# Reset the display to a safe state before exiting.
-lcd.print_line("                ", 0)  # For some reason the clear() function
-lcd.print_line("                ", 1)  # doesn't work, so I print blank lines
-lcd.set_backlight(False)               # instead.
-print ("")
+    lcd.print_line(line0, 0)
+    lcd.print_line(datestr, 1)
