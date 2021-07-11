3394abb561d3f52bc18827f007983f1d971eb77d
chip <chip@example.com>
2021-07-11 18:09:21 -0400

    Finished clock_display & updated ntp.conf

--- clock_display.py
+++ clock_display.py
@@ -14,8 +14,8 @@ from time import sleep, perf_counter
 # Scheduled events - times when certain events should happen. Note that the
 #     dates supplied are just fillers - they are ignored during actual
 #     execution.
-light_on  = datetime(1970, 1, 1,  6,  0, 0)  # Time the backlight should be switched on
-light_off = datetime(1970, 1, 1, 22, 30, 0)  # Time the backlight should be switched off
+light_on  = datetime(1970, 1, 1,  6,  0, 0)  # Backlight ON time
+light_off = datetime(1970, 1, 1, 22, 30, 0)  # Backlight OFF time
 
 
 # FUNCTIONS
@@ -25,9 +25,9 @@ def closeDisplay(sig, frame):
     global active
     active = False
     sleep (1)
-    lcd.print_line("                ", 0)  # For some reason the clear() function
-    lcd.print_line("                ", 1)  # doesn't work, so I print blank lines
-    lcd.set_backlight(False)               # instead.
+    lcd.print_line(" "*16, 0)  # For some reason the clear() function
+    lcd.print_line(" "*16, 1)  # doesn't work, so I print blank lines
+    lcd.set_backlight(False)   # instead.
     print ("")
     sys.exit()
 
@@ -39,8 +39,8 @@ lcd.init()
 # Custom icons for GPS lock & WiFi connection
 char_lock   = (0x04, 0x0a, 0x0a, 0x1f, 0x11, 0x11, 0x1f)
 char_unlock = (0x04, 0x0a, 0x08, 0x1f, 0x11, 0x11, 0x1f)
-char_wifi   = (0x04, 0x04, 0x0a, 0x00, 0x0a, 0x04, 0x04)
-char_nowifi = (0x04, 0x04, 0x0a, 0x0a, 0x04, 0x04, 0x04)
+char_wifi   = (0x04, 0x04, 0x0a, 0x0a, 0x04, 0x04, 0x04)
+char_nowifi = (0x04, 0x04, 0x0a, 0x00, 0x0a, 0x04, 0x04)
 lcd.write_CGRAM(char_lock, 0)
 lcd.write_CGRAM(char_unlock, 1)
 lcd.write_CGRAM(char_wifi, 2)
@@ -53,12 +53,11 @@ now = datetime.now()
 timestr = now.strftime("%H:%M:%S")
 datestr = now.strftime("%b %d, %Y")
 line0 = "INITIALIZING... "
-line1 = "                "
+lcd.print_line(datestr, 1)
 prettyAcc = "  NO ACC"
 active = True
 lockstate = False
 lastlock = perf_counter()
-wifi = False
 
 signal.signal(signal.SIGTERM, closeDisplay)
 signal.signal(signal.SIGINT, closeDisplay)
@@ -70,25 +69,39 @@ while active:
 
     if timestr != now.strftime("%H:%M:%S"):
         timestr = now.strftime("%H:%M:%S")
+
+        # Once-per-minute processing
         if now.strftime("%S") == "00":
+            # Check clock accuracy
             accReport = subprocess.getoutput("ntpstat | grep within | cut -d ' ' -f 8")
             if accReport == "":
                 prettyAcc = "{message: >8}".format(message="NO ACC")
             else:
                 prettyAcc = "{message: >8}".format(message=accReport + "ms")
-
-        if perf_counter() - lastlock > 16:
-            lockstate = False
-
-        if now.strftime("%M:%S") == "00:00":
+            
+            # Check WiFi connection & update icon
+            lcd.move_cursor(1,14)
             if (subprocess.getoutput
                 ("timeout 0.1 ping -c 1 192.168.1.2 | grep time= | head -c 26") ==
                 "64 bytes from 192.168.1.2:"):
-                wifi = True
+                lcd.print(i2clcd.CGRAM_CHR[2])
             else:
-                wifi = False
+                lcd.print(i2clcd.CGRAM_CHR[3])
+            
+            # Check GPS lock & update icon
+            lcd.move_cursor(1,15)
+            if perf_counter() - lastlock > 16:
+                lockstate = False
+            if lockstate:
+                lcd.print(i2clcd.CGRAM_CHR[0])
+            else:
+                lcd.print(i2clcd.CGRAM_CHR[1])
+            
+            # Print date
+            lcd.move_cursor(1,0)
+            lcd.print(now.strftime("%b %d, %Y"))
+
         line0 = timestr + prettyAcc
-        line1 = now.strftime("%b %d, %Y")
 
     if light_on.time() <= now.time() <= light_off.time():
         lcd.set_backlight(True)
@@ -101,19 +114,6 @@ while active:
         lastlock = perf_counter()
 
     lcd.print_line(line0, 0)
-    lcd.print_line(line1, 1)
-
-    lcd.move_cursor(1, 14)
-    if wifi:
-        lcd.print(i2clcd.CGRAM_CHR[2])
-    else:
-        lcd.print(i2clcd.CGRAM_CHR[3])
-        
-    lcd.move_cursor(1, 15)
-    if lockstate:
-        lcd.print(i2clcd.CGRAM_CHR[0])
-    else:
-        lcd.print(i2clcd.CGRAM_CHR[1])
 
 while True:    # Wait to die
     sleep(1)
